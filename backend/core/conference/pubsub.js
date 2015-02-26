1f8a5315c60e616f9ff91c6bb889e2d04962066f
Graham Crosmarie <gcrosmarie@linagora.com>
2015-02-26 17:12:21 +0100

    MEET-66 fix bug : callback function for invitation was not defined

--- backend/core/conference/pubsub.js
+++ backend/core/conference/pubsub.js
@@ -8,9 +8,16 @@ var pubsub = require('../pubsub').local;
  */
 module.exports.init = function(dependencies) {
   var invitation = dependencies('invitation');
+  var logger = dependencies('logger');
 
-  function processInvitation(data, callback) {
-    invitation.sendInvitation(data.creator, data.user, data.conference, data.user.objectType, callback);
+  function processInvitation(data) {
+    invitation.sendInvitation(data.creator, data.user, data.conference, data.user.objectType,
+      function(err, response) {
+        if (err) {
+          return logger.error('Could not send invitation for %s in conference %s', data.user.id, data.conference.id, err);
+        }
+        return logger.debug('Invitation succesfully sent for %s in conference %s', data.user.id, data.conference.id, response);
+      });
   }
 
   pubsub.topic('conference:invite').subscribe(processInvitation);
