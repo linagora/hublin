cfafd80155142c38c5e7026289f7473d2fba0f96
ddolcimascolo <ddolcimascolo@linagora.com>
2015-03-05 10:59:06 +0100

    MEET-39 Fixed indent size

--- backend/core/db/mongo/models/conference.js
+++ backend/core/db/mongo/models/conference.js
@@ -5,29 +5,29 @@ var mongoose = require('mongoose'),
     Tuple = tuple.Tuple;
 
 var TimelineEntrySchema = new mongoose.Schema({
-    verb: {type: String},
-    language: {type: String},
-    published: {type: Date, default: Date.now},
-    actor: {
-        objectType: {type: String, required: true},
-        _id: {type: String, required: true},
-        image: {type: String},
-        displayName: {type: String}
-    },
-    object: {
-        objectType: {type: String},
-        _id: {type: String, required: true}
-    },
-    target: [{
-        objectType: {type: String},
-        _id: {type: String, required: true}
-    }],
-    inReplyTo: [{
-        objectType: {type: String},
-        _id: {type: mongoose.Schema.ObjectId, required: true}
-    }],
-    to: {type: [Tuple], validate: [tuple.validateTuples, 'Bad to tuple']},
-    bto: {type: [Tuple], validate: [tuple.validateTuples, 'Bad to tuple']}
+  verb: {type: String},
+  language: {type: String},
+  published: {type: Date, default: Date.now},
+  actor: {
+    objectType: {type: String, required: true},
+    _id: {type: String, required: true},
+    image: {type: String},
+    displayName: {type: String}
+  },
+  object: {
+    objectType: {type: String},
+    _id: {type: String, required: true}
+  },
+  target: [{
+    objectType: {type: String},
+    _id: {type: String, required: true}
+  }],
+  inReplyTo: [{
+    objectType: {type: String},
+    _id: {type: mongoose.Schema.ObjectId, required: true}
+  }],
+  to: {type: [Tuple], validate: [tuple.validateTuples, 'Bad to tuple']},
+  bto: {type: [Tuple], validate: [tuple.validateTuples, 'Bad to tuple']}
 });
 
 var MemberSchema = new mongoose.Schema({
