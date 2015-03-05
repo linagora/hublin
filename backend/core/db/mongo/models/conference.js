a3926891eac2f7461cd87354e34192de66722fda
Michael Bailly <mbailly@linagora.com>
2015-03-05 13:39:55 +0100

    MEET-40 extract TimelineEntry mongoose schema to its own file

--- backend/core/db/mongo/models/conference.js
+++ backend/core/db/mongo/models/conference.js
@@ -1,34 +1,7 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    tuple = require('../schemas/tuple'),
-    Tuple = tuple.Tuple;
-
-var TimelineEntrySchema = new mongoose.Schema({
-  verb: {type: String},
-  language: {type: String},
-  published: {type: Date, default: Date.now},
-  actor: {
-    objectType: {type: String, required: true},
-    _id: {type: String, required: true},
-    image: {type: String},
-    displayName: {type: String}
-  },
-  object: {
-    objectType: {type: String},
-    _id: {type: String, required: true}
-  },
-  target: [{
-    objectType: {type: String},
-    _id: {type: String, required: true}
-  }],
-  inReplyTo: [{
-    objectType: {type: String},
-    _id: {type: mongoose.Schema.ObjectId, required: true}
-  }],
-  to: {type: [Tuple], validate: [tuple.validateTuples, 'Bad to tuple']},
-  bto: {type: [Tuple], validate: [tuple.validateTuples, 'Bad to tuple']}
-});
+    TimelineEntrySchema = require('../schemas/timelineentry');
 
 var MemberSchema = new mongoose.Schema({
   objectType: {type: String, required: true},
