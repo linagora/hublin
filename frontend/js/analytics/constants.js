ae51297ec0f2da6208137527e6accabdc824cb8c
yannick <yannick.maligoy@gmail.com>
2015-04-27 17:51:53 +0200

    MEET-310 add piwik snippet

---
+++ frontend/js/analytics/constants.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var analytics = {
+  PIWIK_SERVER : '//monitoring.open-paas.org/'
+};
---
+++ frontend/js/analytics/piwik.js
@@ -0,0 +1,9 @@
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  _paq.push(['setTrackerUrl', analytics.PIWIK_SERVER + 'piwik.php']);
+  _paq.push(['setSiteId', 1]);
+  var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+  g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = analytics.PIWIK_SERVER + 'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
