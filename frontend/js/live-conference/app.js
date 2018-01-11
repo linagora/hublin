5ffdd43c735faafa396f55fe50f434607a4ecd6f
Christophe Hamerling <chamerling@linagora.com>
2018-01-11 16:11:24 +0100

    #526 John Papa live conference app

--- frontend/js/live-conference/app.js
+++ frontend/js/live-conference/app.js
@@ -1,71 +1,34 @@
-'use strict';
-
 var angularInjections = angularInjections || [];
 
-angular.module('liveConferenceApplication', [
-  'ngRoute',
-  'ngSanitize',
-  'ngAnimate',
-  'op.socketio',
-  'op.websocket',
-  'op.live-conference',
-  'op.notification',
-  'op.localstorage',
-  'op.dynamicDirective',
-  'meetings.authentication',
-  'meetings.session',
-  'meetings.user',
-  'meetings.conference',
-  'meetings.configuration',
-  'meetings.language',
-  'meetings.i18n',
-  'restangular',
-  'uuid4',
-  'mgcrea.ngStrap',
-  'ngSocial',
-  'matchmedia-ng',
-  'op.live-conference-devmode',
-  'ng.deviceDetector',
-  'angularMoment',
-  'ui.router'
-].concat(angularInjections)).config(function($locationProvider, RestangularProvider, $stateProvider) {
+(function(angular) {
+  'use strict';
 
-  $stateProvider
-    .state('app', {
-      url: '/:conferenceId?displayName&autostart&noAutoInvite',
-      templateUrl: '/views/live-conference/partials/main',
-      controller: 'conferenceController',
-      resolve: {
-        conference: function($stateParams, $location, $log, conferenceService) {
-          var id = $stateParams.conferenceId;
-          return conferenceService.enter(id).then(
-            function(response) {
-              $log.info('Successfully entered room', id, 'with response', response);
-              return response.data;
-            },
-            function(err) {
-              $log.info('Failed to enter room', id, err);
-              $location.path('/');
-            }
-          );
-        }
-      },
-      data: {
-        hasVideo: false
-      }
-    })
-    .state('app.editor-mobile', {
-      templateUrl: '/views/live-conference/partials/conference-mobile-video'})
-    .state('app.conference', {
-      templateUrl: '/views/live-conference/partials/conference-video'});
+  angular.module('liveConferenceApplication', [
+    'ngRoute',
+    'ngSanitize',
+    'ngAnimate',
+    'op.socketio',
+    'op.websocket',
+    'op.live-conference',
+    'op.notification',
+    'op.localstorage',
+    'op.dynamicDirective',
+    'meetings.authentication',
+    'meetings.session',
+    'meetings.user',
+    'meetings.conference',
+    'meetings.configuration',
+    'meetings.language',
+    'meetings.i18n',
+    'restangular',
+    'uuid4',
+    'mgcrea.ngStrap',
+    'ngSocial',
+    'matchmedia-ng',
+    'op.live-conference-devmode',
+    'ng.deviceDetector',
+    'angularMoment',
+    'ui.router'
+  ].concat(angularInjections));
+})(angular);
 
-
-  $locationProvider.html5Mode(true);
-  RestangularProvider.setBaseUrl('/api');
-  RestangularProvider.setFullResponse(true);
-})
-  .run(['$log', 'session', '$state', function($log, session) {
-    session.ready.then(function() {
-      $log.debug('Session is ready.');
-    });
-  }]);
