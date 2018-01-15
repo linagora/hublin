85bfbd5ce57046b8adee80c23e5d6acd0ff34e8d
Christophe Hamerling <chamerling@linagora.com>
2018-01-15 10:54:03 +0100

    #526 Inject constant

--- frontend/js/meetings/app.js
+++ frontend/js/meetings/app.js
@@ -9,6 +9,7 @@ angular.module('meetingsApplication', [
   'meetings.session',
   'meetings.user',
   'meetings.conference',
+  'meetings.conference.constants',
   'meetings.language',
   'mgcrea.ngStrap'
 ]).config(function($routeProvider, RestangularProvider) {
