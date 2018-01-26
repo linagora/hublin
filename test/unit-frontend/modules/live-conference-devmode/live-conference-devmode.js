59f639fd22e0dd991a29ee1a0e4afcbc8903f393
Aymane Khouaji <akhouaji@linagora.com>
2018-01-26 14:19:18 +0100

    #541 test frontend Linters

--- test/unit-frontend/modules/live-conference-devmode/live-conference-devmode.js
+++ test/unit-frontend/modules/live-conference-devmode/live-conference-devmode.js
@@ -17,26 +17,27 @@ describe('The op.live-conference-devmode module', function() {
         getAttendees: function() { return []; }
       };
       self.webRTCService = {
-        myEasyrtcid: function() {},
-        getP2PConnectionStatus: function() {},
+        myEasyrtcid: function() { },
+        getP2PConnectionStatus: function() { },
         doesDataChannelWork: function() { return true; }
       };
     });
 
     describe('enable() method', function() {
       it('should register a callback in $interval', function(done) {
-
         var $interval = function(callback, ms) {
           expect(callback).to.be.a('function');
           expect(ms).to.be.a.number;
           expect(ms).to.equal(1000);
           done();
         };
+
         module(function($provide) {
           $provide.value('currentConferenceState', self.conference);
           $provide.value('webRTCService', self.webRTCService);
           $provide.value('$interval', $interval);
         });
+
         inject(function(devMode) {
           devMode.enable();
         });
@@ -45,27 +46,27 @@ describe('The op.live-conference-devmode module', function() {
 
     describe('setDevModePeerListeners', function() {
       it('should call setPeerListener for all devmodeMsgType values', function(done) {
-
         var handler = 'aHandler';
         var count = 0;
+
         self.webRTCService.setPeerListener = function(listener, msgType) {
           expect(listener).to.deep.equal(handler);
           count++;
           if (count === 1) {
             expect(msgType).to.equal('devmode:sendData');
-          }
-          else if (count === 2) {
+          } else if (count === 2) {
             expect(msgType).to.equal('devmode:sendDataP2P');
-          }
-          else if (count === 3) {
+          } else if (count === 3) {
             expect(msgType).to.equal('devmode:sendDataWS');
             done();
           }
         };
+
         module(function($provide) {
           $provide.value('currentConferenceState', self.conference);
           $provide.value('webRTCService', self.webRTCService);
         });
+
         inject(function(devMode) {
           devMode.setDevModePeerListeners(handler);
         });
@@ -74,19 +75,21 @@ describe('The op.live-conference-devmode module', function() {
   });
   describe('disable() method', function() {
     it('should unregister $interval\'s callback', function(done) {
-
-      var $interval = function(callback, ms) {
+      var $interval = function() {
         return 'token1';
       };
+
       $interval.cancel = function(token) {
         expect(token).to.equal('token1');
         done();
       };
+
       module(function($provide) {
         $provide.value('currentConferenceState', self.conference);
         $provide.value('webRTCService', self.webRTCService);
         $provide.value('$interval', $interval);
       });
+
       inject(function(devMode) {
         devMode.enable();
         devMode.disable();
@@ -111,17 +114,17 @@ describe('The op.live-conference-devmode module', function() {
       };
       self.conference.getAttendees = function() {
         return [
-          {easyrtcid: 'local', displayName: 'localName'},
-          {easyrtcid: 'remote1', displayName: 'remote1Name'},
-          {easyrtcid: 'remote2', displayName: 'remote2Name'}
+          { easyrtcid: 'local', displayName: 'localName' },
+          { easyrtcid: 'remote1', displayName: 'remote1Name' },
+          { easyrtcid: 'remote2', displayName: 'remote2Name' }
         ];
       };
       self.devMode.enable();
       self.$interval.flush(1000);
       expect(self.devMode.attendees).to.deep.equal([
-          {},
-          {easyrtcid: 'remote1', displayName: 'remote1Name', connectionStatusMessage: undefined, connectionStatus: true, dataChannelStatus: true},
-          {easyrtcid: 'remote2', displayName: 'remote2Name', connectionStatusMessage: undefined, connectionStatus: true, dataChannelStatus: true}
+        {},
+        { easyrtcid: 'remote1', displayName: 'remote1Name', connectionStatusMessage: undefined, connectionStatus: true, dataChannelStatus: true },
+        { easyrtcid: 'remote2', displayName: 'remote2Name', connectionStatusMessage: undefined, connectionStatus: true, dataChannelStatus: true }
       ]);
     });
     it('should expose the number of peers', function() {
@@ -130,9 +133,9 @@ describe('The op.live-conference-devmode module', function() {
       };
       self.conference.getAttendees = function() {
         return [
-          {easyrtcid: 'local', displayName: 'localName'},
-          {easyrtcid: 'remote1', displayName: 'remote1Name'},
-          {easyrtcid: 'remote2', displayName: 'remote2Name'}
+          { easyrtcid: 'local', displayName: 'localName' },
+          { easyrtcid: 'remote1', displayName: 'remote1Name' },
+          { easyrtcid: 'remote2', displayName: 'remote2Name' }
         ];
       };
       self.devMode.enable();
@@ -140,7 +143,7 @@ describe('The op.live-conference-devmode module', function() {
       expect(self.devMode.peerCount).to.equal(2);
       self.conference.getAttendees = function() {
         return [
-          {easyrtcid: 'local', displayName: 'localName'}
+          { easyrtcid: 'local', displayName: 'localName' }
         ];
       };
       self.$interval.flush(1000);
